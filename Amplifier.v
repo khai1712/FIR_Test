c83ca453a3fa3eaa413710190840076038a91fdf
PhamVanKhai_20213965 <166353021+khai1712@users.noreply.github.com>
2024-04-13 07:54:49 +0700

    fixed

--- Amplifier.v
+++ Amplifier.v
@@ -4,7 +4,7 @@ module Amplifier (
     input [0:2] amp_coef_1, amp_coef_2, amp_coef_3, amp_coef_4, amp_coef_5, amp_coef_6, amp_coef_7, amp_coef_8,
     output reg [0:15] sig_out_1, sig_out_2, sig_out_3, sig_out_4, sig_out_5, sig_out_6, sig_out_7, sig_out_8
 );
-    reg [0:31] sig_tmp_1, sig_tmp_2, sig_tmp_3, sig_tmp_4, sig_tmp_5, sig_tmp_6, sig_tmp_7, sig_tmp_8;
+    wire [0:31] sig_tmp_1, sig_tmp_2, sig_tmp_3, sig_tmp_4, sig_tmp_5, sig_tmp_6, sig_tmp_7, sig_tmp_8;
     // Scale xuống theo giá trị của hệ số bộ lọc đã nhân lên (2^10)
     assign sig_tmp_1 = (y_out_1[0] == 1'b1) ? (32'hFFC00000 + (y_out_1 >> 10)) : (y_out_1 >> 10);
     assign sig_tmp_2 = (y_out_2[0] == 1'b1) ? (32'hFFC00000 + (y_out_2 >> 10)) : (y_out_2 >> 10);
